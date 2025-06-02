30725ff640addef021335e2df95a4eeb0abe5d37
gustavogaudsena <gaudereto.sena@gmail.com>
2025-06-02 10:09:53 -0300

    Exercício 3

--- CityBreaks.Web/Data/CityBreaksContext.cs
+++ CityBreaks.Web/Data/CityBreaksContext.cs
@@ -6,6 +6,7 @@ namespace CityBreaks.Web.Data;
 public class CityBreaksContext: DbContext
 {
     DbSet<Country> Countries { get; set; }
+    DbSet<City> Cities { get; set; }
     
     public CityBreaksContext(DbContextOptions<CityBreaksContext> options)
         : base(options)
